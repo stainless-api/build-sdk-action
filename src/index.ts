a4c08be15b984ab5a5ccc0f3ed447326ea7b6c6e
meorphis <meorphis@gmail.com>
2025-01-14 17:56:22 -0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -72,7 +72,8 @@ async function main() {
       guessConfig
     }).asResponse();
     const buildId = build.headers.get('X-Stainless-Project-Build-ID');
-    const languages = (build.headers.get('X-Stainless-Project-Build-Languages')?.split(",") || []) as Stainless.Builds.OutputRetrieveParams['target'][]
+    const languageHeader = build.headers.get('X-Stainless-Project-Build-Languages');
+    const languages = (languageHeader?.length ? languageHeader.split(",") : []) as Stainless.Builds.OutputRetrieveParams['target'][]
     if (buildId && languages.length > 0) {
       console.log(`Created build with ID ${buildId} for languages: ${languages.join(", ")}`);
     } else {
