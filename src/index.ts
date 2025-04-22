cd15c6ed48b74f8fd1ed2ae5dbd0418330005722
CJ Quines <cj@cjquines.com>
2025-04-22 14:54:42 -0400

    use v0 api

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { Stainless } from "stainless";
-import { getBooleanInput, getInput, setOutput } from "@actions/core";
+import { StainlessV0 as Stainless } from "stainless";
+import { getInput, setOutput } from "@actions/core";
 import * as fs from "fs";
 import * as path from "path";
-import crypto from "crypto";
 
 // https://www.conventionalcommits.org/en/v1.0.0/
 const CONVENTIONAL_COMMIT_REGEX = new RegExp(
@@ -17,26 +16,56 @@ const MAX_POLLING_SECONDS = 10 * 60; // 10 minutes
 
 async function main() {
   try {
-    const stainless_api_key = getInput("stainless_api_key", { required: true });
-    const projectName = getInput("project_name", { required: true });
-    const oasPath = getInput("oas_path", { required: true });
+    const apiKey = getInput("stainless_api_key", { required: true });
+    const oasPath = getInput("oas_path", { required: false }) || undefined;
     const configPath =
       getInput("config_path", { required: false }) || undefined;
-    const parentOasHash =
-      getInput("parent_oas_hash", { required: false }) || undefined;
-    const parentConfigHash =
-      getInput("parent_config_hash", { required: false }) || undefined;
-    const parentBranch =
-      getInput("parent_branch", { required: false }) || undefined;
+    const projectName = getInput("project_name", { required: true });
+    const commitMessage =
+      getInput("commit_message", { required: false }) || undefined;
     const branch = getInput("branch", { required: false }) || undefined;
     const mergeBranch =
       getInput("merge_branch", { required: false }) || undefined;
-    const commitMessage =
-      getInput("commit_message", { required: false }) || undefined;
-    const guessConfig = getBooleanInput("guess_config", { required: false });
+    const parentRevisionRaw =
+      getInput("parent_revision", { required: false }) || undefined;
+
+    if (mergeBranch && (oasPath || configPath)) {
+      throw new Error(
+        "Cannot specify both merge_branch and oas_path or config_path",
+      );
+    }
+
+    const parentRevisionArray = (() => {
+      if (!parentRevisionRaw) {
+        return [];
+      }
+      const result = JSON.parse(parentRevisionRaw);
+      if (!Array.isArray(result)) {
+        throw new Error("parent_revision must be an array");
+      }
+      if (
+        !result.every(
+          (x) =>
+            typeof x === "string" ||
+            (typeof x === "object" &&
+              Object.entries(x).every(
+                ([key, value]) =>
+                  typeof key === "string" && typeof value === "string",
+              )),
+        )
+      ) {
+        throw new Error(
+          "parent_revision must be an array of strings or objects with string keys and string values",
+        );
+      }
+      if (branch === "main" && result.length > 0) {
+        throw new Error("parent_revision must be empty when branch is 'main'");
+      }
+      return result as Array<string | { [filepath: string]: string }>;
+    })();
 
     const stainless = new Stainless({
-      apiKey: stainless_api_key,
+      apiKey: apiKey,
       logLevel: "warn",
     });
 
@@ -47,18 +76,40 @@ async function main() {
       process.exit(1);
     }
 
+    // Attempt to find a build against a parent revision
     let parentBuildId;
-
-    if (parentBranch) {
-      // attempt to find a parent build
-      const recentBuilds = await stainless.builds.list({
-        project: projectName,
-        spec_hash: parentOasHash,
-        config_hash: parentConfigHash,
-        branch: parentBranch,
-        limit: 1,
-      });
-      parentBuildId = recentBuilds[0]?.id || undefined;
+    for (const parentRevision of parentRevisionArray) {
+      console.log("Searching for build against", parentRevision);
+      if (typeof parentRevision === "string") {
+        parentBuildId = (
+          await stainless.builds.list({
+            project: projectName,
+            revision: parentRevision,
+            limit: 1,
+          })
+        ).data[0]?.id;
+        if (parentBuildId) {
+          break;
+        }
+      } else {
+        parentBuildId = (
+          await stainless.builds.list({
+            project: projectName,
+            revision: Object.fromEntries(
+              Object.entries(parentRevision).map(([key, hash]) => [
+                key,
+                { hash },
+              ]),
+            ),
+            limit: 1,
+          })
+        )?.data[0]?.id;
+        if (parentBuildId) {
+          break;
+        }
+      }
+    }
+    if (parentRevisionArray.length > 0) {
       if (parentBuildId) {
         console.log("Found parent build:", parentBuildId);
       } else {
@@ -66,84 +117,72 @@ async function main() {
       }
     }
 
-    const oasBuffer = fs.readFileSync(oasPath);
+    // If previous build on this branch is not against this revision, hard
+    // reset the branch
+    if (parentBuildId && branch) {
+      const previousBuild = (
+        await stainless.projects.branches.retrieve(branch, {
+          project: projectName,
+        })
+      )?.latest_build;
+      if (previousBuild?.id) {
+        console.log("Previous build against branch found:", previousBuild.id);
+        if (previousBuild.id === parentBuildId) {
+          console.log("Branch already up to date");
+        } else {
+          console.log("Hard resetting branch", branch);
+          await stainless.projects.branches.create(projectName, {
+            branch_from: previousBuild.config_commit,
+            branch: branch,
+          });
+        }
+      }
+    }
+
+    const oasBuffer = oasPath ? fs.readFileSync(oasPath) : undefined;
     const configBuffer = configPath ? fs.readFileSync(configPath) : undefined;
 
     // create a new build
-    const build = await stainless.builds
-      .create({
-        projectName,
-        oasSpec: new File([oasBuffer], path.basename(oasPath), {
-          type: "text/plain",
-          lastModified: fs.statSync(oasPath).mtimeMs,
-        }),
-        stainlessConfig:
-          configPath && configBuffer
-            ? new File([configBuffer], path.basename(configPath), {
-                type: "text/plain",
-                lastModified: fs.statSync(configPath).mtimeMs,
-              })
-            : undefined,
-        parentBuildId,
-        branch,
-        mergeBranch,
-        commitMessage,
-        guessConfig,
-      })
-      .asResponse();
-    let buildId = build.headers.get("X-Stainless-Project-Build-ID");
-    const languageHeader = build.headers.get(
-      "X-Stainless-Project-Build-Languages",
-    );
-    let languages = (
-      languageHeader?.length ? languageHeader.split(",") : []
-    ) as Stainless.Builds.OutputRetrieveParams["target"][];
+    const build = await stainless.builds.create({
+      project: projectName,
+      revision: mergeBranch
+        ? `${branch}..${mergeBranch}`
+        : {
+            ...(oasPath &&
+              oasBuffer && {
+                [path.basename(oasPath)]: {
+                  content: oasBuffer.toString("utf-8"),
+                },
+              }),
+            ...(configPath &&
+              configBuffer && {
+                [path.basename(configPath)]: {
+                  content: configBuffer.toString("utf-8"),
+                },
+              }),
+          },
+      branch,
+      commit_message: commitMessage,
+    });
+    let buildId = build.id;
+    let languages = Object.keys(build.targets) as Array<
+      keyof Stainless.Builds.BuildObject.Targets
+    >;
     if (buildId) {
       console.log(
         `Created build with ID ${buildId} for languages: ${languages.join(", ")}`,
       );
     } else {
-      if (!buildId) {
-        console.log(
-          `No new build was created. Checking for existing builds with the inputs provided...`,
-        );
-        const build = (
-          await stainless.builds.list({
-            project: projectName,
-            spec_hash: crypto.createHash("md5").update(oasBuffer).digest("hex"),
-            config_hash: configBuffer
-              ? crypto.createHash("md5").update(configBuffer).digest("hex")
-              : undefined,
-            branch,
-            limit: 1,
-          })
-        )[0];
-
-        if (build) {
-          buildId = build.id;
-          languages = build.targets;
-          console.log(
-            `Found existing build with ID ${buildId} for languages: ${languages.join(", ")}`,
-          );
-        }
-      }
-
-      if (!buildId) {
-        console.error(
-          "No existing build was found for this branch. Presumably it does not include SDK config changes",
-        );
-        process.exit(0);
-      }
+      console.log(`No new build was created; exiting.`);
+      process.exit(0);
     }
 
     let parentOutcomes: Record<
       string,
-      Stainless.Builds.Outputs.CommitBuildStep.Completed["completed"]
+      Stainless.BuildTarget.Completed.Completed
     > = {};
-    let outcomes: Record<
-      string,
-      Stainless.Builds.Outputs.CommitBuildStep.Completed["completed"]
-    > = {};
+    let outcomes: Record<string, Stainless.BuildTarget.Completed.Completed> =
+      {};
 
     const pollingStart = Date.now();
     while (
@@ -152,38 +191,36 @@ async function main() {
     ) {
       for (const language of languages) {
         if (!(language in parentOutcomes) && parentBuildId) {
-          const parentBuildOutput = await stainless.builds.outputs.retrieve(
-            parentBuildId,
-            { target: language },
-          );
-          if (parentBuildOutput.commit.status === "completed") {
-            const parentOutcome = parentBuildOutput.commit.completed;
+          const parentBuildOutput = (
+            await stainless.builds.retrieve(parentBuildId)
+          ).targets[language];
+          if (parentBuildOutput?.commit.status === "completed") {
+            const parentOutcome = parentBuildOutput?.commit;
             console.log(
               "Parent build completed with outcome:",
               JSON.stringify(parentOutcome),
             );
-            parentOutcomes[language] = parentOutcome;
+            parentOutcomes[language] = parentOutcome.completed;
           } else {
             console.log(
-              `Parent build has status ${parentBuildOutput.commit.status} for ${language}`,
+              `Parent build has status ${parentBuildOutput?.commit.status} for ${language}`,
             );
           }
         }
 
         if (!(language in outcomes)) {
-          const buildOutput = await stainless.builds.outputs.retrieve(buildId, {
-            target: language,
-          });
-          if (buildOutput.commit.status === "completed") {
-            const outcome = buildOutput.commit.completed;
+          const buildOutput = (await stainless.builds.retrieve(buildId))
+            .targets[language];
+          if (buildOutput?.commit.status === "completed") {
+            const outcome = buildOutput?.commit;
             console.log(
               "Build completed with outcome:",
               JSON.stringify(outcome),
             );
-            outcomes[language] = outcome;
+            outcomes[language] = outcome.completed;
           } else {
             console.log(
-              `Build has status ${buildOutput.commit.status} for ${language}`,
+              `Build has status ${buildOutput?.commit.status} for ${language}`,
             );
           }
         }
@@ -199,6 +236,8 @@ async function main() {
     for (const language of languagesWithoutOutcome) {
       outcomes[language] = {
         conclusion: "timed_out",
+        commit: null,
+        merge_conflict_pr: null,
       };
     }
 
@@ -210,4 +249,3 @@ async function main() {
 }
 
 main();
-
